5f1937207cd6d08165f0e5651824c79f778cdf45
elifinesse <133097200+elifinesse@users.noreply.github.com>
2023-07-09 15:11:11 +0300

    character info + crunchy accordion

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
-import React from 'react';
-
+import React from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Karakter from "./components/Karakter";
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [swData, setSwData] = useState([]);
 
+  useEffect(() => {
+    axios
+      .get("https://swapi.dev/api/people/")
+      .then((response) => setSwData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      {swData.length === 0 ? (
+        <p>Loading...</p>
+      ) : (
+        swData.map((character) => (
+          <Karakter data={character} key={character.name} />
+        ))
+      )}
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,29 @@
+import React, { useState } from "react";
+
 // Karakter bileşeniniz buraya gelecek
+
+function Karakter(props) {
+  const { data } = props;
+  const [clicked, setClick] = useState(false);
+  console.log(clicked);
+
+  return (
+    <div className={clicked ? "character-open" : "character-close"}>
+      <div className="character-name" onClick={() => setClick(!clicked)}>
+        <h4>{data.name}</h4>
+      </div>
+      <div className="character-info">
+        <p>Gender: {data.gender}</p>
+        <p>Height: {data.height}</p>
+        <p>Mass: {data.mass}</p>
+        <p>Birth Year: {data.birth_year}</p>
+        <p>Eye Color: {data.eye_color}</p>
+        <p>Hair Color: {data.hair_color}</p>
+        <p>Appears in {data.films.length} films</p>
+        <p>Films: {data.films}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Karakter;
